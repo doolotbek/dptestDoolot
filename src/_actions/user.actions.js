8d80f015d9da862479e131a880f053fe64ee384e
doolot <doolotbek@dreampirates.co.jp>
2018-12-14 21:10:42 +0900

    bug fix

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -92,3 +92,5 @@ function _delete(id) {
     function request(id) { return { type: userConstants.DELETE_REQUEST, id } }
     function success(id) { return { type: userConstants.DELETE_SUCCESS, id } }
     function failure(id, error) { return { type: userConstants.DELETE_FAILURE, id, error } }
+
+}
